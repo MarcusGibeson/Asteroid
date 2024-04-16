e4da41da32abccc20cce8a13498956f7e138aa31
Joshua Whittington <joshwhittington99@gmail.com>
2024-04-16 08:35:52 -0400

    small commit for testing purposes

---
+++ core/src/com/asteroid/game/Asteroid.java
@@ -0,0 +1,16 @@
+package com.asteroid.game;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class Asteroid {
+    private Vector2 position, velocity;
+    private int health;
+    private float rotation, width, height;
+
+    private static final float MAX_SPEED = 5f; //Smallest asteroid size should be roughly the same speed as the player, adjust if needed
+
+    public Asteroid(Vector2 position, Vector2 velocity) {
+        this.position = position;
+        this.velocity = velocity;
+    }
+}
